7e009ed981ffa96022f4c29f67b984baea751644
Beena Nailwal <beena.nailwal@omlogistics.co.in>
2024-08-03 21:10:40 +0530

    file

---
+++ src/File.js
@@ -0,0 +1,18 @@
+function File() {
+    return (
+      <div>this is function component</div>
+    )
+  }
+  
+  export default File
+  
+  
+  // import React , {Component} from 'react'
+  // export default class File extends Component
+  // {
+  //     render () {
+  //         return (
+  //             <h5>this is class component</h5>
+  //         )
+  //     }
+  // }
